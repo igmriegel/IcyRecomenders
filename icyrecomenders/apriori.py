f751df3e9b8bb1ff5bfb4fe7dd238cd5e7c37f0e
Igor Riegel <igor.riegel@gmail.com>
2023-02-04 20:25:05 -0300

    add boilerplate for the scikit methods

--- icyrecomenders/apriori.py
+++ icyrecomenders/apriori.py
@@ -1,2 +1,18 @@
+import polars as pl
+
+
 class Apriori:
-    ...
+    def __init__(self) -> None:
+        pass
+
+    def transform(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
--- icyrecomenders/colaborative_filtering.py
+++ icyrecomenders/colaborative_filtering.py
@@ -1,2 +1,18 @@
+import polars as pl
+
+
 class CollaborativeFiltering:
-    ...
+    def __init__(self) -> None:
+        pass
+
+    def transform(self):
+        pass
+
+    def evaluate(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
